6713c8338fd955968ef178c5eb0937200531d9e7
selva142 <selvagkn@gmail.com>
2024-07-09 12:53:50 +0530

    commit2

--- HelloWorld/src/com/test/HelloWorld.java
+++ HelloWorld/src/com/test/HelloWorld.java
@@ -3,6 +3,7 @@ package com.test;
 public class HelloWorld {
 public static void main(String args[] )
 {
-	System.out.println("welcome to jenkin demo projects");
+	System.out.println("welcome to jenkin demo projects1");
+	
 }
 }
